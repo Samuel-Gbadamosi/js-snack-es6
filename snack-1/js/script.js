d4b737e35205cb8d15d3b53bf2d79f363bbf1811
samuel <damosisamuel@gmail.com>
2021-01-19 14:37:50 +0100

    init

---
+++ snack-1/js/script.js
@@ -0,0 +1,11 @@
+// Utilizzando const e/o let. Inserire un numero, se è pari stampa il numero, se è dispari stampa il numero successivo.
+
+
+let n = parseInt(prompt('insert a number ?'));
+
+
+if (n % 3 == 0){
+  console.log(`${n} pari`);
+} else {
+  console.log(`${n} dispari`);
+}
---
+++ snack-2/js/scripts.js
@@ -0,0 +1,17 @@
+ // Utilizzando const e/o let e template literal. Creare due tag div con due id diversi. Un div avrà il testo colorato di rosso mentre l’altro di verde. Partendo da un array di numeri, stampiamo nell’id rosso i numeri dispari e in verde i numeri pari.
+
+let arrayNumber = [1,2,3,4,5,6,7,8,9,10];
+
+
+
+for (var i = 0; i < arrayNumber.length; i++) {
+console.log(i);
+
+if (arrayNumber[i] % 2 == 0){
+document.getElementById('one').innerHTML +=
+ arrayNumber[i];
+} else {
+  document.getElementById('two').innerHTML += arrayNumber[i];
+
+}
+}
